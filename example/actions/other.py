dabede5b86a893859b51e42d087ddf93a4c8f80c
Matt Davies <matt@teamdavies.co.uk>
2020-07-30 12:49:21 +0100

    update example

--- example/actions/other.py
+++ example/actions/other.py
@@ -1,22 +1,22 @@
 from starlette.responses import PlainTextResponse
 
-class Index:
+class Other:
     def fetch_all(self):
         """Fetch all items"""
-        return PlainTextResponse("Pantam: Index -> fetch_all()")
+        return PlainTextResponse("Pantam: Other -> fetch_all()")
 
     def fetch_single(self, uid):
         """Fetch single item"""
-        return PlainTextResponse("Pantam: Index -> fetch_single()")
+        return PlainTextResponse("Pantam: Other -> fetch_single()")
 
     def create(self, data):
         """Create an item"""
-        return PlainTextResponse("Pantam: Index -> create()")
+        return PlainTextResponse("Pantam: Other -> create()")
 
     def update(self, uid, data):
         """Update an item"""
-        return PlainTextResponse("Pantam: Index -> update()")
+        return PlainTextResponse("Pantam: Other -> update()")
 
     def delete(self, uid):
         """Delete single item"""
-        return PlainTextResponse("Pantam: Index -> delete()")
+        return PlainTextResponse("Pantam: Other -> delete()")
