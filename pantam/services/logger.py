c4d9bfeaf2df5c5b563fb79721f7e4967db52e02
Matt Davies <matt@teamdavies.co.uk>
2020-08-06 16:08:46 +0100

    make use of built in logging

--- pantam/services/logger.py
+++ pantam/services/logger.py
@@ -1,31 +1,28 @@
-from typing import Callable, Optional
-from sys import stdout
+import logging
+from os import environ
 from colored import fg, attr
+
+logging.basicConfig(level=environ.get("PANTAM_LOG_LEVEL", "INFO"))
 
 PANTAM: str = fg("yellow") + attr("bold") + "PANTAM: " + attr("reset")
 
-WriteStream = Callable[[str], int]
-
 
 class Logger:
-    def __init__(self, write_stream: Optional[WriteStream] = None) -> None:
-        if write_stream is None:
-            self.write_stream = stdout.write
-        else:
-            self.write_stream = write_stream
-
-    def __write(self, message: str) -> None:
-        """Write message to stdout."""
-        self.write_stream(str(PANTAM + message) + "\n")
+    def __init__(self) -> None:
+        self.info_logger = logging.getLogger("pantam.info")
+        self.success_logger = logging.getLogger("pantam.success")
+        self.error_logger = logging.getLogger("pantam.error")
 
     def info(self, message: str) -> None:
         """Print info message."""
-        self.__write(fg("blue") + attr("bold") + message + attr("reset"))
+        self.info_logger(PANTAM + fg("blue") + attr("bold") + message + attr("reset"))
 
     def success(self, message: str) -> None:
         """Print success message"""
-        self.__write(fg("green") + attr("bold") + message + attr("reset"))
+        self.success_logger(
+            PANTAM + fg("green") + attr("bold") + message + attr("reset")
+        )
 
     def error(self, message: str) -> None:
         """Print error message"""
-        self.__write(fg("red") + attr("bold") + message + attr("reset"))
+        self.error_logger(PANTAM + fg("red") + attr("bold") + message + attr("reset"))
